0d2e05dcb6fe27ff383cb307e8b3c30bd2bc25de
jfelds28 <justinfeldstein87@gmail.com>
2026-04-30 14:56:55 -0400

    added player interact detector

--- final_project_all_together_now/Scripts/Player1.gd
+++ final_project_all_together_now/Scripts/Player1.gd
@@ -7,9 +7,12 @@ extends CharacterBody2D
 #we might wanna have gravity/jump height an export var-
 #in case we wanna make different levels with diff-
 #gravity/jump height
-
 const JUMP_VELOCITY = -400.0
 const GRAVITY = 980.0
+
+#sets up interactables detector
+var interactables = []
+
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
@@ -25,4 +28,15 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, player_speed)
 	
+	if Input.is_action_just_pressed("Interact"):
+		if interactables:
+			interactables.back().interact()
 	move_and_slide()
+
+
+func _on_interactable_detector_area_entered(area: Area2D) -> void:
+	interactables.append(area)
+
+
+func _on_interactable_detector_area_exited(area: Area2D) -> void:
+	interactables.erase(area)
