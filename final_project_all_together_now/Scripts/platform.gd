bdae0cf4c23229b78b4fb23a1b642bf8a248ab21
mcheng11 <mcheng0439@gmail.com>
2026-05-08 00:11:20 -0400

    reworked the moving platform
    ill show how it works tomorrow

--- final_project_all_together_now/Scripts/platform.gd
+++ final_project_all_together_now/Scripts/platform.gd
@@ -1,43 +1,16 @@
-class_name MovingPlatform extends Node2D
+extends Path2D
 
-@export var speed: float = 100.0
-@export var loop: bool = true
+@onready var follow_path: PathFollow2D = $PathFollow2D
 
-var waypoints: Array[Vector2] = []
-var current_waypoint: int = 0
-var direction: int = 1
+@export var speed: =0.2
 
-func _ready():
-	# Grab all Marker2D children and use their positions as waypoints
-	for child in get_children():
-		if child is Marker2D:
-			waypoints.append(child.global_position)
+var fowardDirection = 1
+
+func _physics_process(delta: float) -> void:
+	follow_path.progress_ratio += speed * delta * fowardDirection
 	
-	if waypoints.size() < 2:
-		print("WARNING: MovingPlatform needs at least 2 Marker2D children!")
-		return
+	if fowardDirection == 1 and follow_path.progress_ratio == 1:
+		fowardDirection = -1
 	
-	global_position = waypoints[0]
-
-func _physics_process(delta: float):
-	if waypoints.size() < 2:
-		return
-
-	var target = waypoints[current_waypoint]
-	global_position = global_position.move_toward(target, speed * delta)
-
-	if global_position.distance_to(target) < 1.0:
-		_advance_waypoint()
-
-func _advance_waypoint():
-	current_waypoint += direction
-
-	if loop:
-		if current_waypoint >= waypoints.size():
-			direction = -1
-			current_waypoint = waypoints.size() - 2
-		elif current_waypoint < 0:
-			direction = 1
-			current_waypoint = 1
-	else:
-		current_waypoint = clamp(current_waypoint, 0, waypoints.size() - 1)
+	elif fowardDirection == -1 and follow_path.progress_ratio == 0:
+		fowardDirection = 1
